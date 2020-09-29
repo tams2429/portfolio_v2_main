f58dd7561f34bef1e167d4e6fe229d9bd98e72e6
Siu Kei Tam <tam.s.k.92@gmail.com>
2020-09-29 23:15:55 +0100

    Added Title & Responsive design without animation

--- frontend/src/sections/Home.js
+++ frontend/src/sections/Home.js
@@ -2,10 +2,16 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className="home-container" id="home" style={{height: '1000px'}}>
-      <h1>This is the Home component</h1>
+    <div className="home-container" id="home" >
+      <div className="box-border">
+        <div className="titles">
+            <p>Hi,
+              <br/>My name is <strong className='name'>Siu Kei</strong>
+              <br/>I'm a <em><span className='typing'></span></em>
+            </p>
+        </div>
+      </div>
     </div>
-
   )
 }
 
