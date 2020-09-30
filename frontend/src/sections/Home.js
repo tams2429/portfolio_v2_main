395031623ba811a5271a13322e05cb472e54643a
Siu Kei Tam <tam.s.k.92@gmail.com>
2020-09-30 19:57:53 +0100

    Added title border animation and button

--- frontend/src/sections/Home.js
+++ frontend/src/sections/Home.js
@@ -1,14 +1,64 @@
 import React from 'react'
 
-const Home = () => {
+const texts = ['Programmer', 'Coffee Addict', 'Whisky Enthusiast', 'Outside-The-Box Thinker']
+  let element = 0
+  let index = 0
+  let currentText = null
+  let letter = null
+
+function Home() {
+  const [variableText, setvariableText] = React.useState('')
+
+  React.useEffect(() => {
+    const erase = () => {
+      if(index === currentText.length+1) {
+        setTimeout(erase, 1000)
+        letter = currentText.slice(0, --index)
+        setvariableText(letter)
+      } else if(index > 0){
+        letter = currentText.slice(0, --index)
+        console.log(letter)
+        setvariableText(letter)
+        setTimeout(erase, 100)
+      } else{
+        element++
+        setTimeout(type, 400)
+      }
+    }
+    const type = () => {
+      currentText = texts[element]
+      letter = currentText.slice(0, index++)
+      setvariableText(letter)
+      if (letter.length === currentText.length && element !== texts.length-1) {
+        erase()
+      } else if(element === texts.length-1) {
+          if(index <= currentText.length) {
+              setTimeout(type, 400)
+          }
+          return
+      } else {
+          setTimeout(type, 400)
+      }
+    }
+    type()
+  }, [])
+
   return (
     <div className="home-container" id="home" >
       <div className="box-border">
         <div className="titles">
             <p>Hi,
-              <br/>My name is <strong className='name'>Siu Kei</strong>
-              <br/>I'm a <em><span className='typing'></span></em>
+              <br/>My name is <strong className='name'>Siu Kei</strong>,
+              <br/>I'm a <em><span className='typing'>{variableText}</span></em>
             </p>
+        </div>
+        <div className="button-container">
+          <button className="start-button">
+            LET'S GET STARTED
+          </button>
+        </div>
+        <div className="icon-container">
+
         </div>
       </div>
     </div>
